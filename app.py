9f7c2549ccfe0e4a334c7b18c26ccfbf873eb1d1
MichaelMAB <michael.a.baicker.17@dartmouth.edu>
2016-02-08 13:19:19 -0500

    added comments to authenticate, changed POST response to 201

--- app.py
+++ app.py
@@ -22,29 +22,32 @@ class MongoEncoder(JSONEncoder):
         else:
             return JSONEncoder.default(obj, **kwargs)
 
+
+# Adds Authentification for GET and POST requests
 class AuthMiddleware(object):
 
+    # Checks for X-Auth-Token and X-Project ID Headers
     def process_request(self, req, resp):
         token = req.get_header('X-Auth-Token')
         project = req.get_header('X-Project-ID')
 
+        # Asks for token if none is provided
         if token is None:
             description = ('Please provide an auth token '
                            'as part of the request.')
 
-            raise falcon.HTTPUnauthorized('Auth token required',
-                                          description,
-                                          href='http://docs.example.com/auth')
+            #Raises 401 Unauthorized Error
+            raise falcon.HTTPUnauthorized('Auth token required', description)
 
+        # Responds to invalid X-Auth-Token and/or X-Project ID
         if not self._token_is_valid(token, project):
             description = ('The provided auth token is not valid. '
                            'Please request a new token and try again.')
 
-            raise falcon.HTTPUnauthorized('Authentication required',
-                                          description,
-                                          href='http://docs.example.com/auth',
-                                          scheme='Token; UUID')
+            # Raises 401 Unauthorized Error
+            raise falcon.HTTPUnauthorized('Authentication required', description)
 
+    # Checks X-Auth-Token and X-Project-ID values
     def _token_is_valid(self, token, project):
         if token == 'Michael' and project == "test":
             return True  # Suuuuuure it's valid...
@@ -108,7 +111,7 @@ class Sciurus(object):
     def on_post(self, req, resp, **kwargs):
         obj = req.context['doc']
         result = self.items.insert_one(obj)
-        resp.status = falcon.HTTP_200  # This is the default status
+        resp.status = falcon.HTTP_201  # Successful POST
         resp.body = {
             'result': 'You did it! Here is the id:{0}'.format(result),
         }
